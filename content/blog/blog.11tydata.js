7e4d5e40616fa8e1abbb9093bb929b9869394c5e
bradallenfisher <bradallenfisher@gmail.com>
2023-11-03 09:32:46 -0400

    undefined

--- content/blog/blog.11tydata.js
+++ content/blog/blog.11tydata.js
@@ -3,4 +3,11 @@ module.exports = {
 		"posts"
 	],
 	"layout": "layouts/post.njk",
+	sitename: "House Lyft",
+    sitedesc: "Elevating Home Standards.",
+    siteurl: "https://houselyft.com",
+    siteimgsrc: "/img/logo.png",
+    siteimgwidth: "500",
+    siteimgheight: "500",
+    author: "Katie Rossi"
 };
